ff020d0a7dac939b616cd32f09c91e20ff544844
Sif Fahim <siffahim25@gmail.com>
2022-01-08 22:27:06 +0600

    add style

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Slide from 'react-reveal/Slide';
 import Zoom from 'react-reveal/Zoom';
+import { Typewriter } from 'react-simple-typewriter';
 import img from '../../../images/fahim.png';
 import './Banner.css';
 
@@ -13,16 +14,18 @@ const Banner = () => {
                     <Col sm={12} md={7} className='pt-4 order-2 order-md-1 '>
                         <Zoom>
                             <p className='text-muted'>WELCOME TO MY WORLD</p>
-                            <h1 style={{ fontSize: '48px', fontWeight: 'bold' }}>Hi, I’m <sapn style={{ color: '#f9004d' }}>𝐒𝐀𝐈𝐅𝐔𝐋 𝐈𝐒𝐋𝐀𝐌</sapn>
-                                <div className='wrapper'>
-                                    <div className='static-txt'>a</div>
-                                    <ul className='daynamic-txt'>
-                                        <li><span>Mern Stack Developer</span></li>
-                                        <li><span>Front End Developer</span></li>
-                                        <li><span>Full Stack Developer</span></li>
-                                        <li><span>Back End Developer</span></li>
-                                    </ul>
-                                </div>
+                            <h1 style={{ fontSize: '40px', fontWeight: 'bold' }}>Hi,<br /> I’M <sapn style={{ color: '#f9004d' }}>𝐒𝐀𝐈𝐅𝐔𝐋 𝐈𝐒𝐋𝐀𝐌</sapn><br />
+                                A <span style={{ color: '#f9004d' }}>
+                                    <Typewriter
+                                        loop
+                                        cursor
+                                        cursorStyle='|'
+                                        typeSpeed={70}
+                                        deleteSpeed={50}
+                                        delaySpeed={1000}
+                                        words={['MERN Stack Developer', 'Frontend Developer', 'Full Stack Developer', 'Junior Web Developer']}
+                                    />
+                                </span>
                             </h1>
 
                             <p className='mb-5 text-muted'>A highly skilled developer with strong confidence with high quality and high-performance web applications seeks the rules of junior Software Developer at JavaScript, React, Node, andmodern JavaScript libraries.</p>
--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
-import Filp from 'react-reveal/Slide';
+import { Bounce } from 'react-reveal';
 import { Link } from 'react-router-dom';
 
 const Project = ({ project }) => {
     const { img1, name, id } = project;
     return (
         <Col sm={12} md={4}>
-            <Filp left>
+            <Bounce bottom>
                 <div className='project-card'>
                     <div className='overflow-hidden rounded'>
                         <img src={img1} alt="" />
@@ -15,7 +15,7 @@ const Project = ({ project }) => {
                     <p className='txt'>{name}</p>
                     <Link to={`/detail/${id}`} className='text-project fs-4'><p>This service provide for design <span className='link-project'><i className="fas fa-external-link-alt"></i></span></p></Link>
                 </div>
-            </Filp>
+            </Bounce>
         </Col>
     );
 };
