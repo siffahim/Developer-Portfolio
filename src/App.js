0e778c8d209307209690e1a5ecfa320b9997f8e8
Fahim <siffahim25@gmail.com>
2022-04-19 17:15:41 +0600

    cursor style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AnimatedCursor from "react-animated-cursor";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Pages/Blogs/Blogs';
@@ -8,20 +9,28 @@ import Media from './Pages/Media/Media';
 import NotFound from './Pages/NotFound/NotFound';
 import Navigation from './Pages/Share/Navigation/Navigation';
 
-
 function App() {
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path={`/detail/:detailId`} element={<Detail />} />
-        <Route path='/blog' element={<Blogs />} />
-        <Route path='/media' element={<Media />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path={`/detail/:detailId`} element={<Detail />} />
+          <Route path='/blog' element={<Blogs />} />
+          <Route path='/media' element={<Media />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </Router>
+      {/* cursor style */}
+      <AnimatedCursor
+        innerSize={10}
+        outerSize={8}
+        color='249, 0, 77'
+      />
+
+    </>
   );
 }
 
