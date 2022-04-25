9aeeb4ff4c1ef7282259569b3bdd47d65fb2506c
Fahim <siffahim25@gmail.com>
2022-04-25 23:33:09 +0600

    update project

--- src/Pages/Home/Projects/Projects.js
+++ src/Pages/Home/Projects/Projects.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Slide from 'react-reveal/Slide';
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import Project from '../Project/Project';
+import { A11y, Autoplay, EffectCoverflow, Navigation, Pagination, Scrollbar } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+// swiper bundle styles
+import 'swiper/swiper-bundle.min.css';
+// swiper core styles
+import 'swiper/swiper.min.css';
 import './Projects.css';
+
 
 const Projects = () => {
     const [projects, setProjects] = useState([]);
@@ -21,16 +27,51 @@ const Projects = () => {
                     <h3 className="text fw-bold" style={{ color: "#f6f6f6" }}>My Portfolio</h3>
                 </div>
             </Slide>
-            <Container>
-                <Row className='g-2'>
-                    {
-                        projects.map(project => <Project
-                            key={project.id}
-                            project={project}
-                        />)
-                    }
-                </Row>
-            </Container>
+            <Row className='g-2'>
+                <Col xs={12} md={12}>
+                    <Swiper
+                        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCoverflow]}
+                        autoplay={{ delay: 3000 }}
+                        effect={"coverflow"}
+                        slidesPerView={1}
+                        breakpoints={{
+                            688: {
+                                slidesPerView: 3
+                            }
+                        }}
+                        spaceBetween={15}
+                        coverflowEffect={{
+                            rotate: 50,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 1,
+                            slideShadows: true,
+                        }}
+                        pagination={{ clickable: true }}
+                    >
+                        {
+                            projects.map(product => <SwiperSlide>
+                                <button className='card-btn'>
+                                    <div className='project-card text-start'>
+                                        <div className='overflow-hidden'>
+                                            <img src={product.img1} alt="" />
+                                        </div>
+                                        <div className='text-start'>
+                                            <span style={{ background: '#6e7ff7', color: '#fff' }} className='title-card'>React</span>
+                                            <span style={{ background: '#4ee56f', color: '#fff' }} className='title-card'>Node</span>
+                                            <span style={{ background: '#ffac2c ', color: '#fff' }} className='title-card'>Firebase</span>
+                                            <span style={{ background: '#4ee56f', color: '#fff' }} className='title-card'>MongoDb</span>
+                                            <span style={{ background: '#ff205d', color: '#fff' }} className='title-card'>Firebase</span>
+                                        </div>
+                                        <h5 className='text-light'>{product.name}</h5>
+                                        <p className='text-light text-justify'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                                    </div>
+                                </button>
+                            </SwiperSlide>)
+                        }
+                    </Swiper>
+                </Col>
+            </Row>
         </div>
     );
 };
