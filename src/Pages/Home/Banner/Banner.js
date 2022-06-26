f8c97a524657edf78d71098ee35dfcb27126513e
Fahim <siffahim25@gmail.com>
2022-06-26 19:27:52 +0600

    icon change

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Typewriter } from 'react-simple-typewriter';
 import img from '../../../images/fahim.png';
@@ -28,14 +27,14 @@ const Banner = () => {
                         <p className='mb-5 text-muted'>A highly skilled developer with strong confidence with high quality and high-performance web applications seeks the rules of junior Software Developer at JavaScript, React, Node, andmodern JavaScript libraries.</p>
                         <div>
                             <p>FIND WITH ME</p>
-                            <a data-aos="zoom-in-down" rel='noreferrer' href="https://www.facebook.com/sif.fahim" target='_blank' className='text-light'>
-                                <div className='banner-icon'><i className="fab fa-facebook-f"></i></div>
-                            </a>
-                            <a data-aos="zoom-in-down" rel="noreferrer" href="https://www.instagram.com/siffahim/" target='_blank' className='text-light'>
-                                <div className='banner-icon'><i className="fab fa-instagram"></i></div>
+                            <a data-aos="zoom-in-down" rel="noreferrer" href="https://github.com/siffahim" target='_blank' className='text-light'>
+                                <div className='banner-icon'><i className="fa-brands fa-github"></i></div>
                             </a>
                             <a data-aos="zoom-in-down" rel="noreferrer" href="https://www.linkedin.com/in/siffahim/" target='_blank' className='text-light'>
                                 <div className='banner-icon'><i className="fab fa-linkedin-in"></i></div>
+                            </a>
+                            <a data-aos="zoom-in-down" rel='noreferrer' href="https://www.facebook.com/sif.fahim" target='_blank' className='text-light'>
+                                <div className='banner-icon'><i className="fab fa-facebook-f"></i></div>
                             </a>
                         </div>
                     </Col>
